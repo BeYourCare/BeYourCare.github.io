3db91d3647ae5ba9dba6d1b4194471cb05549017
budiyono <budiyono@nexsoft.co.id>
2021-06-20 19:34:54 +0700

    add link

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+
+function gotoWhatsapp(){
+    window.open("https://wa.me/62083126919704","_blank")
+}
+
+function gotoShopee(){
+    window.open("https:/https://shopee.co.id/nviananggitia","_blank")
+}
